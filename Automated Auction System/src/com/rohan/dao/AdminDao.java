dd06be555540162d24863924e5b88ad0b4dcde90
rohan209547mourya <rohan.209547@gmail.com>
2022-09-28 17:08:11 +0530

    Java project added

---
+++ Automated Auction System/src/com/rohan/dao/AdminDao.java
@@ -0,0 +1,9 @@
+package com.rohan.dao;
+
+import com.rohan.beans.Admin;
+
+public interface AdminDao {
+	
+	public void adminLogIn(Admin admin);
+	
+}
---
+++ Automated Auction System/src/com/rohan/utility/GetConnection.java
@@ -0,0 +1,47 @@
+package com.rohan.utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class GetConnection {
+	
+	public static Connection get() {
+		
+		
+		Connection conn = null;
+		
+		try {
+			
+			Class.forName("com.mysql.cd.jdbc.Driver");
+		}
+		catch(ClassNotFoundException e) {
+			
+			System.out.println(e.getMessage());
+		}
+		
+		
+		
+		String url = "jdbc://mysql://localhost:3306/auction_main";
+		
+		try {
+			
+			
+			conn = DriverManager.getConnection(url , "root" , "rohan@2004");
+			
+			
+		}
+		catch(SQLException e) {
+			
+			System.out.println("ERROR 202 : Unable to Connect with server");
+			
+		}
+		
+		
+		
+		
+		return conn;
+	}
+	
+}
