6ca8456521c7a5f46cff4f00e16e6210e54b6bd8
rohan209547mourya <rohan.209547@gmail.com>
2022-09-30 09:28:01 +0530

    View products

---
+++ Automated Auction System/src/com/rohan/exception/ProductException.java
@@ -0,0 +1,14 @@
+package com.rohan.exception;
+
+public class ProductException extends Exception{
+
+	public ProductException() {
+		
+	}
+	
+	
+	public ProductException(String mess) {
+		
+		super(mess);
+	}
+}
--- Automated Auction System/src/com/rohan/usecase/UserBuyer.java
+++ Automated Auction System/src/com/rohan/usecase/UserBuyer.java
@@ -4,12 +4,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.rohan.beans.Buyer;
 import com.rohan.beans.Product;
 import com.rohan.dao.BuyerDao;
 import com.rohan.exception.BuyerException;
+import com.rohan.exception.ProductException;
 import com.rohan.utility.GetConnection;
 
 public class UserBuyer implements BuyerDao{
@@ -18,8 +20,34 @@ public class UserBuyer implements BuyerDao{
 	public void registerAsBuyer(Buyer user) {
 		
 		
-		
-		
+		try(Connection conn = GetConnection.get()) {
+			
+			
+			PreparedStatement state = conn.prepareStatement("insert into buyers(buyer_name , buyer_username , buyer_password) values (? , ? , ?)");
+			
+			state.setString(1, user.getName());
+			state.setString(2, user.getUsername());
+			state.setString(3, user.getPassword());
+			
+			if(state.executeUpdate() > 0) {
+				
+				System.out.println("User registered successfully!");
+			}
+			else {
+				
+				throw new BuyerException("User already registered!");
+			}		
+			
+		}
+		catch(SQLException e) {
+			
+			System.out.println(e.getMessage());
+			
+		} 
+		catch (BuyerException e) {
+			
+			System.out.println(e.getMessage());
+		}
 		
 	}
 	
@@ -70,8 +98,56 @@ public class UserBuyer implements BuyerDao{
 
 	@Override
 	public List<Product> viewByCategory(String cate) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		List<Product> products = new ArrayList<>();
+		
+		try(Connection conn = GetConnection.get()) {
+			
+			
+		    PreparedStatement state = conn.prepareStatement("select * from products where category = ?");
+			state.setString(1, cate);
+			
+			ResultSet res = state.executeQuery();
+			
+			while(res.next()) {
+				
+				
+				int i = res.getInt("product_id");
+				String n = res.getString("product_name");
+				int p = res.getInt("base_price");
+				String s = res.getString("status");
+				String c = res.getString("category");
+				
+				
+				String s1 = "Sold";
+				
+				if(s.equalsIgnoreCase("N")) {
+					
+					s1 = "Available";
+				}
+				
+				
+				products.add(new Product(i, p, n, s1, c));
+				
+			}
+			
+			
+			if(products.size() == 0) {
+				
+				throw new ProductException("Product not available for this category!");
+			}
+			
+		}
+		catch(SQLException e) {
+			
+			System.out.println(e.getMessage());
+		} 
+		catch (ProductException e) {
+			
+			System.out.println(e.getMessage());
+		}
+		
+		return products;
 	}
 
 	@Override
